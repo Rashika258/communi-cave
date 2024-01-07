43e2277d73d3c4ad00e8654d98b35cc21c8f0eef
Rashika258 <rashikasuresh5758@gmail.com>
2024-01-07 17:55:18 +0530

    Server Modal

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import { UploadDropzone } from "@/lib/uploadthing";
-import {FileIcon, X} from 'lucide-react';
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
-
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
@@ -12,59 +11,50 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endPoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
 
-    const fileType = value?.split('.').pop();
-    if (value && fileType !== "pdf") {
-        return (
-          <div className="relative h-20 w-20">
-            <Image
-              fill
-              src={value}
-              alt="Upload"
-              className="rounded-full"
-            />
-            <button
-              onClick={() => onChange("")}
-              className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
-              type="button"
-            >
-              <X className="h-4 w-4" />
-            </button>
-          </div>
-        )
-      }
-
-
-      if (value && fileType === "pdf") {
-        return (
-          <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
-            <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
-            <a 
-              href={value}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
-            >
-              {value}
-            </a>
-            <button
-              onClick={() => onChange("")}
-              className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
-              type="button"
-            >
-              <X className="h-4 w-4" />
-            </button>
-          </div>
-        )
-      }
-    
-    
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
 
   return (
     <UploadDropzone
-      endpoint={'imageUploader'}
+      endpoint={"imageUploader"}
       onClientUploadComplete={(res) => {
-        console.log('res', res)
+        console.log("res", res);
         onChange(res?.[0]?.url);
       }}
       onUploadError={(error: Error) => {
---
+++ components/navigation/navigation-action.tsx
@@ -0,0 +1,12 @@
+const NavigationAction = () => {
+    return ( <div>
+        <button>
+            <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center">
+
+            </div>
+
+        </button>
+    </div> );
+}
+ 
+export default NavigationAction;
