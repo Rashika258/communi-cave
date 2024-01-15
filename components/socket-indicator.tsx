6f097160e1c7f287876ceb85c381d2801cc3d879
Rashika258 <rashikasuresh5758@gmail.com>
2024-01-15 08:13:37 +0530

    Socket Indicator created

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from "react";
+import { useSocket } from "@/components/providers/socket-provider";
+import { Badge } from "./ui/badge";
 
 const SocketIndicator = () => {
-    const {} = useSo
+  const { isConnected } = useSocket();
+
+  if (!isConnected) {
+    return (
+      <Badge
+        variant={"outline"}
+        className="bg-yellow-600 text-white border-none"
+      >
+        Fallback: Polling every 1s
+      </Badge>
+    );
+  }
+
   return (
-    <div>
-      
-    </div>
+    <Badge variant="outline" className="bg-emerald-600 text-white border-none">
+      Live: Real-time updates
+    </Badge>
   );
-}
+};
 
 export default SocketIndicator;
