00da54a7cf1bfb72e0bcf0dd7c4da236aff5676f
Rashika258 <rashikasuresh5758@gmail.com>
2024-01-18 07:39:54 +0530

    Live kit configuration

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -3,7 +3,12 @@
 import { useUser } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
 import React, { useEffect, useState } from "react";
-import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import {
+    LiveKitRoom,
+    VideoConference,
+    GridLayout,
+    ParticipantTile,
+  } from '@livekit/components-react';
 import "@livekit/components-styles";
 
 interface MediaRoomProps {
--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { io as ClientIO } from "socket.io-client";
 
